4fca938b830b8ec6f463f1bebb375fac0216aa69
ra890927 <happy.890927410@gmail.com>
2021-11-30 14:51:42 +0800

    fix ball repeat surface

--- hw03/shape-def.js
+++ hw03/shape-def.js
@@ -14,36 +14,38 @@ ball_vertices = () => {
 
     const R = 1;
     var vertices = [];
-    for(let out_angle = 0; out_angle < 360; out_angle += 3){
+    for(let out_angle = -90; out_angle < 90; out_angle += 10){
         let right_zdirection = R * Math.sin(out_angle * Math.PI / 180);
-        let left_zdirection = R * Math.sin((out_angle + 3) * Math.PI / 180);
+        let left_zdirection = R * Math.sin((out_angle + 10) * Math.PI / 180);
         let right_proj_radius = Math.abs(R * Math.cos(out_angle * Math.PI / 180));
-        let left_prog_radius = Math.abs(R * Math.cos((out_angle + 3) * Math.PI / 180));
+        let left_prog_radius = Math.abs(R * Math.cos((out_angle + 10) * Math.PI / 180));
 
-        for(let in_angle = 0; in_angle < 360; in_angle += 3){
+        for(let in_angle = 0; in_angle < 360; in_angle += 10){
             // the right bottom vertice
             let right_bottom_x = right_proj_radius * Math.cos(in_angle * Math.PI / 180);
             let right_bottom_y = right_proj_radius * Math.sin(in_angle * Math.PI / 180);
 
             // the right top vertice
-            let right_top_x = right_proj_radius * Math.cos((in_angle + 3) * Math.PI / 180);
-            let right_top_y = right_proj_radius * Math.sin((in_angle + 3) * Math.PI / 180);
+            let right_top_x = right_proj_radius * Math.cos((in_angle + 10) * Math.PI / 180);
+            let right_top_y = right_proj_radius * Math.sin((in_angle + 10) * Math.PI / 180);
 
             // the left bottom vertice
             let left_bottom_x = left_prog_radius * Math.cos(in_angle * Math.PI / 180);
             let left_bottom_y = left_prog_radius * Math.sin(in_angle * Math.PI / 180);
 
             // the left top vertice
-            let left_top_x = left_prog_radius * Math.cos((in_angle + 3) * Math.PI / 180);
-            let left_top_y = left_prog_radius * Math.sin((in_angle + 3) * Math.PI / 180);
+            let left_top_x = left_prog_radius * Math.cos((in_angle + 10) * Math.PI / 180);
+            let left_top_y = left_prog_radius * Math.sin((in_angle + 10) * Math.PI / 180);
 
             vertices = vertices.concat([
+                right_top_x, right_top_y, right_zdirection,
                 left_top_x, left_top_y, left_zdirection,
+                left_bottom_x, left_bottom_y, left_zdirection,
+                
                 right_top_x, right_top_y, right_zdirection,
                 left_bottom_x, left_bottom_y, left_zdirection,
-                right_top_x, right_top_y, right_zdirection,
-                left_bottom_x, left_bottom_y, left_zdirection,
-                right_bottom_x, right_bottom_y, right_zdirection
+                right_bottom_x, right_bottom_y, right_zdirection,
+                
             ]);
         }
     }
@@ -54,12 +56,12 @@ ball_vertices = () => {
 cylinder_vertices = () => {
     const R = 1;
     var vertices = [];
-    for(let angle = 0; angle < 360; angle += 3){
+    for(let angle = 0; angle < 360; angle += 10){
         let left_x = R * Math.cos(angle * Math.PI / 180);
         let left_y = R * Math.sin(angle * Math.PI / 180);
 
-        let right_x = R * Math.cos((angle + 3) * Math.PI / 180);
-        let right_y = R * Math.sin((angle + 3) * Math.PI / 180);
+        let right_x = R * Math.cos((angle + 10) * Math.PI / 180);
+        let right_y = R * Math.sin((angle + 10) * Math.PI / 180);
 
         vertices = vertices.concat([
             // top circle
@@ -68,9 +70,9 @@ cylinder_vertices = () => {
             right_x, right_y, 1,
 
             // side rectangle
-            left_x, left_y, 1,
-            right_x, right_y, 1,
             left_x, left_y, -1,
+            right_x, right_y, 1,
+            left_x, left_y, 1,
             right_x, right_y, 1,
             left_x, left_y, -1,
             right_x, right_y, -1,
